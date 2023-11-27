1a50db84e4e427d8037f113e7649a08fdb0dbb28
ammarasimeurus <148402457+ammarasimeurus@users.noreply.github.com>
2023-11-28 00:14:09 +0500

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,8 +1,15 @@
-CREATE DATABASE responses;
-CREATE USER 'pyapp'@'localhost' IDENTIFIED BY '123_abcde';
-GRANT ALL PRIVILEGES ON responses.* TO 'pyapp'@'localhost';
+CREATE DATABASE IF NOT EXISTS responses;
+
+-- Check if the user 'pyapp'@'localhost' exists before creating
+CREATE USER IF NOT EXISTS 'pyapp'@'localhost' IDENTIFIED BY '123_abcde';
+
+-- Grant privileges only if the user was just created or does not have them
+GRANT ALL PRIVILEGES ON responses.* TO 'pyapp'@'localhost' WITH GRANT OPTION;
+
+-- Make sure privileges take effect
 FLUSH PRIVILEGES;
-CREATE TABLE url_responses (
+
+CREATE TABLE IF NOT EXISTS url_responses (
     id INT AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255) NOT NULL,
     status_code INT NOT NULL
